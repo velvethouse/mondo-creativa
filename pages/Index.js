31deb4be2aadcc5e39f55cef6c49da0410c26ca1
velvethouse <velvethouse@velvethouseagency.com>
2025-09-26 21:45:51 +0200

    Create Index.js

---
+++ pages/Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>🌍 Bienvenue sur Mondo Creativa</h1>
+      <p>Films, musique, littérature, cuisine, art, podcasts et performances.</p>
+    </div>
+  );
+    }
